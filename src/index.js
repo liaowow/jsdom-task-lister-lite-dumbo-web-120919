c317d06078f9ed33e61fd9999594003e5a4b9346
Annie Liao <liaowow@gmail.com>
2020-01-29 15:56:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // 1. elements we are listening for
+  const newTaskForm = document.querySelector("#create-task-form");
+  const newTaskContent = document.querySelector("#new-task-description");
+
+  
+  // 2. listen to the 'submit' event
+  newTaskForm.addEventListener("submit", handleFormSubmit);
+
+  // 3. do something about the 'submit' event
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    // create new to-do item
+    const newToDo = document.createElement('li');
+    // grab the task content and assign it as the innertext of the to-do item
+    newToDo.innerText = newTaskContent.value;
+
+    // add the item to MyToDo
+    function addMyToDo(newToDo) {
+      document.querySelector('#tasks').appendChild(newToDo);
+    }
+
+    addMyToDo(newToDo);
+    event.target.reset();
+  }
+
 });
+
